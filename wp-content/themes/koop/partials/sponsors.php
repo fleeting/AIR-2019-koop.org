74df4e616e40613b1efaf2b29bf2adfbc99214d5
James Fleeting <twofivethreetwo@gmail.com>
2020-01-05 13:18:03 -0600

    Updating GravityForms. Adding website link to Sponsors.

--- wp-content/themes/koop/partials/sponsors.php
+++ wp-content/themes/koop/partials/sponsors.php
@@ -11,14 +11,18 @@
       <div class="col-12 logos">
 
         <?php $args = array(
-        		'post_type' => 'Sponsors',
-        		'posts_per_page' => -1,
-        	);
-          $loop = new WP_Query( $args );
-        	 while ( $loop->have_posts() ) : $loop->the_post();
-           $logo = get_field('sponsor_logo');?>
+          'post_type' => 'Sponsors',
+          'posts_per_page' => -1,
+        );
 
-           <img src="<?= $logo['url']; ?>" alt="<?= $logo['alt']; ?>">
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post();
+          $logo = get_field('sponsor_logo');
+          $website = get_field('website'); ?>
+
+          <?php if(!empty($website)) { ?><a href="<?= $website; ?>" title="Visit <?= get_the_title(); ?>"><?php } ?>
+          <img src="<?= $logo['url']; ?>" alt="<?= $logo['alt']; ?>">
+          <?php if(!empty($website)) { ?></a><?php } ?>
 
         <?php endwhile; wp_reset_postdata(); ?>
       </div>
